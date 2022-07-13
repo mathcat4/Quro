9085e3f5b3c9722af8947c8fdd2b3684f3f49710
Tams-Tams <Tams.Mathe@gmail.com>
2022-07-13 06:59:59 +0200

    do tokenizer done

--- curo/tokenizer.js
+++ curo/tokenizer.js
@@ -15,40 +15,101 @@ Token.prototype.toString = function () {
 
 class TokenType {
   static NUMBER = "number";
+  static CURO = "curo";
+  static STR_DELIM = "string";
 }
 
 class IsType {
   static number(char) {
     return !!char.match(/\d/);
   }
-  static element(char) {}
+  static curo(char) {
+    return elements.includes(char);
+  }
+  static str_delim(char) {
+    return char == '"';
+  }
+  static unic(char) {
+    return unics.includes(char);
+  }
 }
 
 function tokenize(code) {
   var current_num = false;
+  var current_str = false;
+  var current_dec = false;
+
   var number = "";
+  var string = "";
+  var decimal = "";
   var tokens = [];
 
   for (let char of code) {
     if (!IsType.number(char)) {
-      if (current_num) {
-        tokens.push(new Token(TokenType.NUMBER, number));
+      if (char == DEC) {
+        decimal += number + ".";
+
+        current_dec = true;
+        current_num = false; // so the next digits get added to `number`
+        number = "";
+      } else if (current_dec) {
+        decimal += number;
+        if (decimal.startsWith(".")) {
+          decimal = "0" + decimal;
+        }
+        if (decimal.endsWith(".")) {
+          decimal += "5";
+        }
+
+        tokens.push(new Token(TokenType.NUMBER, parseFloat(decimal)));
+        current_dec = current_num = false;
+        number = decimal = "";
+      } else if (current_num) {
+        tokens.push(new Token(TokenType.NUMBER, parseInt(number)));
         current_num = false;
         number = "";
       }
-    } else {
+    }
+
+    if (!IsType.str_delim(char) && current_str) {
+      string += char;
+    } else if (IsType.curo(char)) {
+      tokens.push(new Token(TokenType.CURO, char));
+    } else if (IsType.number(char)) {
       number += char;
       if (!current_num) {
         current_num = true;
       }
+    } else if (IsType.str_delim(char)) {
+      if (!current_str) {
+        current_str = true;
+      } else {
+        tokens.push(new Token(TokenType.STRING, string, (string = true)));
+        current_str = false;
+        string = "";
+      }
     }
   }
 
-  if (current_num) {
-    tokens.push(new Token(TokenType.NUMBER, number));
+  if (current_dec) {
+    decimal += number;
+    if (decimal.startsWith(".")) {
+      decimal + "0" + decimal;
+    }
+    if (decimal.endsWith(".")) {
+      decimal += "5";
+    }
+
+    tokens.push(new Token(TokenType.NUMBER, parseFloat(decimal)));
+  } else if (current_num) {
+    tokens.push(new Token(TokenType.NUMBER, parseInt(number)));
+  }
+
+  if (current_str) {
+    tokens.push(new Token(TokenType.STRING, string, (string = true)));
   }
 
   return tokens;
 }
 
-// console.log(tokenize("123 234 "));
+// console.log(tokenize("123 234 "))
--- documents/estoric.md
+++ documents/estoric.md
@@ -1,5 +1,24 @@
-1. TFBCS
-.Curo uses a three-fourth-byte-character set, with 6 bits and 64 combinations.
-2. Varying arity
-Every element has different arities, the arity will be passed as an argument.
-3. I had many more but I forgor.
+This just a file to note down my ideas.
+# Arity classes
+`a+` -> Addition, arity 2 (default)
+`b+` -> Summation, arity 1
+`c+` -> 
+# If statements
+`1I"Yeah"]"Nah"]`
+<code>
+1I"Yeah"]"Nah"]
+1                Push 1
+ I      ]     ]  If statement
+  "Yeah"         If true, push "Yeah"
+         "Nah"   If false, push "Nah"
+</code>
+
+# Fibonacci
+`L+cVS]`
+<code>
+L+cvS]
+L    ] Lambda
+ +     Add 2 args
+  cV   Constant [0, 1]
+    S  Generate Infinite Sequence
+</code>
